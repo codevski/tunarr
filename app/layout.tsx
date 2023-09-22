5bb413467fff4a0b95a45e8497bcee7cde36ed74
codevski <1435321+codevski@users.noreply.github.com>
2023-09-23 00:17:37 +1000

    :sparkles: add modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
-import Sidebar from "@/components/Sidebar";
 import "./globals.css";
+
 import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
+
+import Sidebar from "@/components/Sidebar";
+import SupabaseProvider from "@/providers/SupabaseProviders";
+import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -18,7 +23,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Sidebar>{children}</Sidebar>
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider />
+            <Sidebar>{children}</Sidebar>
+          </UserProvider>
+        </SupabaseProvider>
       </body>
     </html>
   );
---
+++ providers/UserProvider.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { MyUserContextProvider } from "@/hooks/useUser";
+
+interface UserProviderProps {
+  children: React.ReactNode;
+};
+
+const UserProvider = ({ children }: UserProviderProps) => {
+  return (
+    <MyUserContextProvider>{children}</MyUserContextProvider>
+  )
+}
+
+export default UserProvider;
